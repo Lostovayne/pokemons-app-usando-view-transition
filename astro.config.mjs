54d7458db309988b87ae58047264b713069efeef
LostoVayne <moises.barrera.dev@gmail.com>
2024-06-17 19:24:23 -0400

    agrega alias para manejar el src/*

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import { resolve } from "path";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": resolve("./src"),
+      },
+    },
+  },
+});
